a192e6c22046ce8ca32670f92f28d321b3a1f069
Luca Moroni <terraformer.144@gmail.com>
2021-05-31 22:48:30 +0200

    rework multidimensional hyperboloid and disk (to review)

--- pymanopt/manifolds/hyperboloid.py
+++ pymanopt/manifolds/hyperboloid.py
@@ -6,76 +6,128 @@ from pymanopt.manifolds.manifold import Manifold
 
 
 class Hyperboloid(Manifold):
-    def __init__(self, dimension):
+    def __init__(self, n, k):
+        self.n = n
+        self.k = k
+        self.dimension = n * k
         super().__init__(
-            self, "Hyperboloid over R^{}:1".format(dimension), dimension
+            "{} Hyperboloid over R^{}:1".format(k, n), self.dimension,
             )
 
-    def _minkowski_dot(self, X, Y):
-        return np.dot(X[:-1], Y[:-1]) - X[-1]*Y[-1]
+    def _squeeze(self, X):
+        if self.k == 1 and len(X.shape) > 1:
+            return np.squeeze(X, axis=1)
+        else:
+            return X
+
+    def _pack(self, X):
+      if len(X.shape) == 1:
+        return np.expand_dims(X, axis=1)
+      else:
+        return X
+
+    def inner_minkowski_columns(self, U, V):
+        U = self._pack(U)
+        V = self._pack(V)
+        return np.array(
+              [
+                  np.dot(U[:, i][:-1], V[:, i][:-1]) - U[:, i][-1]*V[:, i][-1]
+                  for i in range(self.k)
+              ]
+              )
 
     def typicaldist(self):
         return math.sqrt(self.dim)
 
-    def inner(self, X, G, H):
-        return self._minkowski_dot(G, H)
+    def inner(self, X, U, V):
+        U = self._pack(U)
+        V = self._pack(V)
+        return np.sum(self.inner_minkowski_columns(U, V))
 
     def proj(self, X, G):
-        inner = self._minkowski_dot(X, G)
-        return G + X*inner
+        X = self._pack(X)
+        G = self._pack(G)
+        inners = self.inner_minkowski_columns(X, G)
+        return self._squeeze(G + X*inners)
 
     def norm(self, X, G):
-        return math.sqrt(max(0, self._minkowski_dot(G, G)))
+        return math.sqrt(max(0, self.inner(G, G)))
 
     def rand(self):
-        ret = np.zeros(self.dim+1)
-        x0 = np.random.normal(self.dim)
-        x1 = math.sqrt(1 + np.dot(x0))
-        ret[:-1] = x0
-        ret[-1] = x1
-        return ret
+        ret = np.zeros((self.n+1, self.k))
+        x0 = np.random.normal(size=(self.n, self.k))
+        x1 = np.sqrt(1 + np.sum(x0 * x0, axis=0))
+        ret[:-1, :] = x0
+        ret[-1, :] = x1
+        return self._squeeze(ret)
 
     def randvec(self, X):
+        X = self._pack(X)
         U = self.proj(X, np.random.rand(X.shape))
-        return U / self.norm(X, U)
+        return self._squeeze(U / self.norm(X, U))
 
     def zerovec(self, X):
         return np.zeros(X.shape)
 
-    def dist(self, X, Y):
-        alpha = max(1, -self._minkowski_dot(X, Y))
+    def _dists(self, X, Y):
+        X = self._pack(X)
+        Y = self._pack(Y)
+        alpha = -self.inner_minkowski_columns(X, Y)
+        alpha[alpha < 1] = 1
         return np.arccosh(alpha)
 
+    def dist(self, X, Y):
+        X = self._pack(X)
+        Y = self._pack(Y)
+        return la.norm(self._dists(X, Y))
+
     def egrad2rgrad(self, X, G):
-        G[-1] = -G[-1]
-        return self.proj(X, G)
+        X = self._pack(X)
+        G = self._pack(G)
+        G[-1, :] = -G[-1, :]
+        return self._squeeze(self.proj(X, G))
 
     def ehess2rhess(self, X, G, H, U):
-        G[-1] = -G[-1]
-        H[:, -1] = -H[:, -1]
-        inners = self._minkowski_dot(X, G)
-        return self.proj(X, U*inners + H)
+        X = self._pack(X)
+        G = self._pack(G)
+        H = self._pack(H)
+        U = self._pack(U)
+        G[-1, :] = -G[-1, :]
+        H[-1, :] = -H[-1, :]
+        inners = self.inner_minkowski_columns(X, G)
+        return self._squeeze(self.proj(X, U*inners + H))
 
     def retr(self, X, U):
-        return self.exp(X, U)
+        X = self._pack(X)
+        U = self._pack(U)
+        return self._squeeze(self.exp(X, U))
 
     def exp(self, X, U):
-        mink_norm_u = self.norm(X, U)
-        a = np.sinh(mink_norm_u)/mink_norm_u
-        if mink_norm_u == 0:
-            a = 1
-        return np.cosh(mink_norm_u)*X + U*a
+        X = self._pack(X)
+        U = self._pack(U)
+        # compute the individual minkowski norm for each individual column of U
+        mink_inners = self.inner_minkowski_columns(U, U)
+        vnormmf = np.vectorize(lambda x: math.sqrt(max(0, x)))
+        mink_norms = vnormmf(mink_inners)
+        a = np.sinh(mink_norms)/mink_norms
+        a[np.isnan(a)] = 1
+        return self._squeeze(np.cosh(mink_norms)*X + U*a)
 
     def log(self, X, Y):
-        alpha = max(1, -self._minkowski_dot(X, Y))
-        if alpha == 1:
-            a = 1
-        else:
-            a = np.arccosh(alpha)/((alpha**2 - 1)**(1/2))
-        return (a)*(Y - alpha*X)
+        X = self._pack(X)
+        Y = self._pack(Y)
+        d = self._dists(X, Y)
+        a = d/np.sinh(d)
+        a[np.isnan(a)] = 1
+        return self._squeeze(self.proj(X, Y*a))
 
     def transp(self, X1, X2, G):
-        return self.proj(X2, G)
+        X1 = self._pack(X1)
+        X2 = self._pack(X2)
+        G = self._pack(G)
+        return self._squeeze(self.proj(X2, G))
 
     def pairmean(self, X, Y):
-        return self.exp(X, self.log(X, Y), 1/2)
+        X = self._pack(X)
+        Y = self._pack(Y)
+        return self._squeeze(self.exp(X, self.log(X, Y), 1/2))
--- pymanopt/manifolds/poincare_ball.py
+++ pymanopt/manifolds/poincare_ball.py
@@ -1,5 +1,4 @@
 import math
-from random import random
 
 import numpy as np
 import numpy.linalg as la
@@ -8,24 +7,24 @@ from pymanopt.manifolds.manifold import Manifold
 
 
 class PoincareBall(Manifold):
-    def __init__(self, dimension):
+    def __init__(self, k, n):
+        self.k = k
+        self.n = n
+        self.dimension = k*n
         super().__init__(
-            "PoincareBall over R^{}".format(dimension), dimension
+            "{} PoincareBall over R^{}".format(self.k, self.n), self.dimension,
             )
 
-    def conformal_factor(self, x):
-        return 2/(1 - np.dot(x, x))
+    def conformal_factor(self, X):
+        return 2/(1 - np.sum(X*X, axis=0))
 
     def mobius_add(self, x, y):
-        x_dot_y = np.dot(x, y)
-        x_norm_q = np.dot(x, x)
-        y_norm_q = np.dot(y, y)
+        x_dot_y = np.sum(x*y, axis=0)
+        x_norm_q = np.sum(x*x, axis=0)
+        y_norm_q = np.sum(y*y, axis=0)
 
         num = (1 + 2*x_dot_y + y_norm_q)*x + (1 - x_norm_q)*y
         den = 1 + 2*x_dot_y + x_norm_q*y_norm_q
-
-        if den == 0:
-            den = 10e-14
 
         return num/den
 
@@ -33,24 +32,24 @@ class PoincareBall(Manifold):
         return self.dim / 8
 
     def inner(self, X, G, H):
-        return self.conformal_factor(X)**2*np.dot(G, H)
+        return np.sum(np.sum(G*H, axis=0) * self.conformal_factor(X)**2)
 
     def proj(self, X, G):
         # Identity map since the embedding space is the tangent space R^n
         return G
 
     def norm(self, X, G):
-        return self.conformal_factor(X)*la.norm(G)
+        return math.sqrt(self.inner(X, G, G))
 
     def rand(self):
-        isotropic = np.random.rand(self.dim)
+        isotropic = np.random.standard_normal(self.n, self.k)
         isotropic = isotropic / la.norm(isotropic)
-        radius = random() ** (1 / self.dim)
+        radius = np.random.rand(self.k) ** (1 / self.n)
         x = isotropic * radius
         return x
 
     def randvec(self, X):
-        v = np.random.rand(self.dim)
+        v = np.random.rand(self.n, self.k)
         v = v / self.norm(X, v)
         return v
 
@@ -58,9 +57,14 @@ class PoincareBall(Manifold):
         return np.zeros(X.shape)
 
     def dist(self, X, Y):
-        a = max(1, 1 + 2*(np.dot(X-Y, X-Y)
-                / ((1-np.dot(X, X))*(1-np.dot(Y, Y)))))
-        return np.arccosh(a)
+        norms2x = np.sum(X*X, axis=0)
+        norms2y = np.sum(Y*Y, axis=0)
+        norms2diff = np.sum((X - Y)*(X - Y), axis=0)
+        a = max(
+            1,
+            1 + 2*(norms2diff / ((1-norms2x)*(1-norms2y))),
+            )
+        return math.sqrt(sum(np.arccosh(a)**2))
 
     def egrad2rgrad(self, X, G):
         factor_q = self.conformal_factor(X)**2
@@ -68,23 +72,24 @@ class PoincareBall(Manifold):
 
     def ehess2rhess(self, X, G, H, U):
         factor = self.conformal_factor(X)
-        return (U * np.dot(G, X) - G * np.dot(U, X) - X * np.dot(U, G)
-                + H/factor)/factor
+        return (U * np.sum(G*X, axis=0) - G * np.sum(U*X, axis=0)
+                - X * np.sum(U*G, axis=0) + H/factor)/factor
 
     def retr(self, X, U):
         return self.exp(X, U)
 
     def exp(self, X, U):
         norm_u = la.norm(U)
+        factor = (1 - np.sum(X*X, axis=0))
         # avoid division by 0
-        tmp = np.tanh(1/2*self.norm(X, U)) * (U/((norm_u + (norm_u == 0))))
+        tmp = np.tanh(norm_u/factor) * (U/((norm_u + (norm_u == 0))))
         return self.mobius_add(X, tmp)
 
     def log(self, X, Y):
         a = self.mobius_add(-X, Y)
-        b = math.sqrt(np.dot(a, a))
-        c = 2/self.conformal_factor(X)*np.arctanh(b)
-        return c*a/b
+        b = la.norm(a)
+        factor = 1 - np.sum(X*X, axis=0)
+        return a * factor * np.arctanh(b) / b
 
     def transp(self, X1, X2, G):
         return G
